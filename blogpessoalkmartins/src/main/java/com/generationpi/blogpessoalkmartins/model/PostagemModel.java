aa9c6edbce36ec81c1cd5c50ef6b5e667045e0ef
kyanne-martins <kyanne801@gmail.com>
2022-08-03 21:23:09 -0300

    Criação do CRUD e das classes tema

--- blogpessoalkmartins/src/main/java/com/generationpi/blogpessoalkmartins/model/PostagemModel.java
+++ blogpessoalkmartins/src/main/java/com/generationpi/blogpessoalkmartins/model/PostagemModel.java
@@ -6,12 +6,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name= "tb_postagens")
@@ -32,6 +35,20 @@ public class PostagemModel {
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private TemaModel tema;
+	
+	
+
+	public TemaModel getTema() {
+		return tema;
+	}
+
+	public void setTema(TemaModel tema) {
+		this.tema = tema;
+	}
 
 	public Long getId() {
 		return id;
